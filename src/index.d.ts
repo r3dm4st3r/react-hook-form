113d4699581edfe95468f9d31ab11c2cceb369af
r3dm4st3r <prafullaondemand@gmail.com>
2024-01-18 01:59:40 +0530

    Import re-arranged

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,27 +1,47 @@
-export { default as Input } from "./elements/input/Input";
 export { default as Checkbox } from "./elements/checkbox/Checkbox";
-export { default as Password } from "./elements/password/PasswordInput";
-export { default as RadioGroup } from "./elements/radio/RadioGroup";
-export { default as Select } from "./elements/select/Select";
-export { default as Switch } from "./elements/switch/Switch";
-export { default as Textarea } from "./elements/textarea/Textarea";
 export { default as Chip } from "./elements/chip/Chip";
 export { default as ChipGroup } from "./elements/chip/ChipGroup";
 export { default as ColorInput } from "./elements/color/ColorInput";
 export { default as ColorPicker } from "./elements/color/ColorPicker";
+export { default as FileInput } from "./elements/file/FileInput";
+export { default as Input } from "./elements/input/Input";
+export { default as JsonInput } from "./elements/json/JsonInput";
+export { default as NumberInput } from "./elements/number/NumberInput";
+export { default as Password } from "./elements/password/PasswordInput";
+export { default as PinInput } from "./elements/pin/PinInput";
+export { default as RadioGroup } from "./elements/radio/RadioGroup";
+export { default as Rating } from "./elements/rating/Rating";
+export { default as SegmentedControl } from "./elements/segment/SegmentedControl";
+export { default as Select } from "./elements/select/Select";
+export { default as NativeSelect } from "./elements/select/NativeSelect";
+export { default as Slider } from "./elements/slider/Slider";
+export { default as RangeSlider } from "./elements/slider/RangeSlider";
+export { default as Switch } from "./elements/switch/Switch";
+export { default as SwitchGroup } from "./elements/switch/SwitchGroup";
+export { default as Textarea } from "./elements/textarea/Textarea";
 
 export type {
-  InputProps,
-  ICheckbox,
-  IRadioGroup,
-  ISwitch,
-  ITextArea,
-  IPassword,
-  ISelect,
   IBaseForm,
-  ILabelValue,
+  ICheckbox,
   IChip,
   IChipGroup,
   IColorInput,
   IColorPicker,
+  IFileInput,
+  IJsonInput,
+  ILabelValue,
+  INativeSelect,
+  INumberInput,
+  InputProps,
+  IPassword,
+  IPinInput,
+  IRadioGroup,
+  IRating,
+  IRangeSlider,
+  ISegmentedControl,
+  ISlider,
+  ISwitch,
+  ISwitchGroup,
+  ITextArea,
+  ISelect,
 } from "./types";
